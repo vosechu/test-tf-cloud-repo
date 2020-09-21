e9902efba28cd6aa6912e6467975580392c5abaf
Chuck Lauer Vose <vosechu@gmail.com>
2020-09-21 13:46:23 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "newrelic" {}
 
-resource "newrelic_alert_channel" "slack channel" {
-  name = "slack"
-  type = "slack"
+resource "newrelic_alert_channel" "foo" {
+  name = "meeeee"
+  type = "email"
 
   config {
-    url = ""
-    channel = ""
+    recipients              = "vosechu@gmail.com"
+    include_json_attachment = "1"
   }
 }
